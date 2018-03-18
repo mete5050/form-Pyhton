bda0691d95b7a0cf2e463de3646956da292f4168
mete5050 <33295066+mete5050@users.noreply.github.com>
2018-03-18 12:15:11 +0300

    Create form.py

---
+++ form.py
@@ -0,0 +1,30 @@
+bg=[]
+Ad="Ad : "
+Soyad="Soyad : "
+Adresiniz="Adres : "
+Telefon_Numaraniz="Telefon Numaranız : "
+Ek_Bilgiler ="Ek Bilgiler : "
+a=input("Adınız :")
+bg.append(Ad+a)
+sa=input("Soyadınız :")
+bg.append(Soyad+sa)
+ckt = bg[0]+bg[1]+".txt"
+dosya = open(ckt, "a")
+adr=input("Adresiniz :")
+bg.append(Adresiniz+adr)
+tel=input("Telefon Numaranız :")
+bg.append(Telefon_Numaraniz+tel)
+eb=input("Ek Bilgiler : ")
+bg.append(Ek_Bilgiler+eb)
+i = 0
+while i<=4:
+	yz = bg[i] +"\n"
+	ckt = a+sa+".txt"
+	dosya = open(ckt, "a")
+	dosya.write(yz)
+	print(bg[i])
+	i+=1
+dosya.close()
+dosya = open(ckt, "r")
+a = str(dosya.read())
+print(a)
